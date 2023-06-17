98d025bd66c5c455a331abc46ed13213f07d3892
Ciekce <kantaja98@gmail.com>
2023-06-17 23:17:21 +0100

    lol
    Bench: 11410427

--- src/main.cpp
+++ src/main.cpp
@@ -18,41 +18,11 @@
 
 #include "uci.h"
 #include "bench.h"
-#include "eval/material.h"
 
 using namespace polaris;
 
 int main(int argc, const char *argv[])
 {
-	/*
-	std::cout << "constexpr auto MvvLva = std::array {";
-
-	for (i32 moving = 0; moving < 6; ++moving)
-	{
-		if (moving > 0)
-			std::cout << ",";
-		std::cout << "\n\tstd::array {\n\t\t";
-
-		const auto srcValue = eval::pieceValue(static_cast<BasePiece>(moving)).midgame();
-
-		for (i32 captured = 0; captured <= 6; ++captured)
-		{
-			if (captured > 0)
-				std::cout << ", ";
-
-			const auto dstValue = eval::pieceValue(static_cast<BasePiece>(captured)).midgame();
-			std::cout << (captured == static_cast<i32>(BasePiece::King)
-				? 0 : ((dstValue - srcValue) * 2000 + dstValue));
-		}
-
-		std::cout << "\n\t}";
-	}
-
-	std::cout << "\n};";
-
-	return 0;
-	 */
-
 	if (argc > 1 && std::string{argv[1]} == "bench")
 	{
 		search::Searcher searcher{16};
